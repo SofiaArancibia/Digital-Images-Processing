1997a9e8b0dce8f59102574b8ec133864a93c190
SofiaArancibia <sofi.aran_05@hotmail.com>
2021-02-07 12:17:54 -0300

    Add basics in image handling

---
+++ Basics/main.cpp
@@ -0,0 +1,93 @@
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "../pdi_functions-master/pdi_functions.h"
+#include "MisFunciones.h"
+#include <vector>
+
+
+using namespace cv;
+using namespace std;
+using namespace pdi;
+
+int main(int argc, char** argv){
+	//create a gui window:
+//	namedWindow("Output",1); //ventana para mostrar la imagen
+	//initialize a 120X350 matrix of black pixels:
+	//Mat output = Mat::zeros( 120, 350, CV_8UC(3) ); //crear imagen 
+	
+	/// Ejercicio 1
+	
+//	Mat output = imread("C:\\Users\\ribeiro\\Desktop\\PDI- 2017\\Imagenes\\billete.jpg");
+//	Mat output = imread("C:\\Users\\ribeiro\\Desktop\\PDI- 2017\\Imagenes\\chairs.jpg");
+//	imshow("Output", output);
+	
+//	int filas = output.rows;
+//	int columnas = output.cols;
+	
+//	cout<<"Cantidad de columnas: "<<columnas<<endl;
+//	cout<<"Cantidad de filas: "<<filas<<endl;
+//	cout<<"Cantidad de canales: "<<output.channels()<<endl;
+//	cout<<"Profundidad: "<<output.depth()<<endl;
+//	cout<<"Size: "<<output.size()<<endl;
+
+	//write text on the matrix:
+//	putText(output,
+//			"Hello World :)",
+//			cvPoint(15,70),
+//			FONT_HERSHEY_PLAIN,
+//			3,
+//			cvScalar(0,255,0),
+//			4);
+	
+	//La imagen se almacena como una matriz de filas, columnas y canales
+	//Los pixeles se almacenan en orden BGR
+	
+	//Recortar imagen
+	
+	//Setup a rectangle to define your ROI
+//	Rect myROI(100,100,200,200);
+//		
+//	///CROP the Image
+//	Mat croppedImage = output(myROI);
+	
+	//Show the croppedImage
+//	namedWindow("Cropped Image",1); //ventana para mostrar la imagen
+//	imshow("Cropped Image", croppedImage);
+
+	///varias imagenes en una sola ventana : en readme.txt
+	
+	///Ejercicio 2
+	
+	//Si abro la imagen en escala de grises puedo decir que lo veo son valores de intensidad
+	
+	Mat botellas = imread("C:\\Users\\ribeiro\\Desktop\\PDI- 2017\\Imagenes\\Botellas_test\\3.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+//	Mat botellas = imread("C:\\Users\\ribeiro\\Desktop\\PDI- 2017\\Imagenes\\botellas.tif",CV_LOAD_IMAGE_GRAYSCALE);
+//	Mat botellas = imread("C:\\Users\\ribeiro\\Desktop\\PDI- 2017\\Imagenes\\botellas.tif");
+		
+	std::cout<<"Filas: "<<botellas.rows<<std::endl;
+	std::cout<<"Columnas: "<<botellas.cols<<std::endl;
+	
+	namedWindow("Botellas",1); //ventana para mostrar la imagen
+	imshow("Botellas", botellas);
+	
+	///IMPORTANTE: ver como se pasan los parametros extra en el callback
+	setMouseCallback("Botellas", misFunciones::capturar_ROI,(void*)&botellas);
+	
+//	misFunciones::nivel_de_llenado_botella(botellas);
+	
+	//paso f100, puede ser cualquier otra...
+	vector<int>intensidades;
+	misFunciones::PerfilDeIntensidad(botellas,"f100",false,intensidades);
+	
+	
+	
+
+	
+	
+	
+	
+
+	//wait for the user to press any key:
+	waitKey(0);
+	return 0;
+}
---
+++ Basics/readme.txt
@@ -0,0 +1,43 @@
+*VARIAS IMAGENES EN UNA SOLA VENTANA
+
+//Leo varias imagenes y las guardo en un vector
+	
+	Mat image4 = imread("C:\\Users\\ribeiro\\Desktop\\PDI- 2017\\Imagenes\\huang1.jpg",CV_8UC1);
+	Mat image1 = imread("C:\\Users\\ribeiro\\Desktop\\PDI- 2017\\Imagenes\\chairs.jpg",CV_8UC1);	
+	Mat image2 = imread("C:\\Users\\ribeiro\\Desktop\\PDI- 2017\\Imagenes\\futbol.jpg",CV_8UC1);
+	Mat image3 = imread("C:\\Users\\ribeiro\\Desktop\\PDI- 2017\\Imagenes\\ratoncillo.jpg",CV_8UC1);
+	
+	Mat model = Mat::zeros( 400, 600, CV_8UC(1) ); //crear imagen para copiar tamaño (creo que es al pedo)
+	
+	vector<Mat> images;
+//Guardo las imagenes en el vector
+	images.push_back(image1);
+	images.push_back(image2);
+	images.push_back(image3);
+	images.push_back(image4);
+	
+///RESIZING: Cambio el tamaño para que todas tengan el mismo (el del modelo)
+	for (int i=0; i<images.size(); i++){
+		resize(images[i],images[i],model.size());
+	}
+
+//Creo el mosaico
+	cv::Mat image_array = pdi::mosaic(images,2);
+
+
+------------------------------------------------------------------------------------------------------------------
+
+*GUARDAR IMAGEN
+
+///GUARDAR la imagen
+	vector<int> compression_params;
+	compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+	imwrite("C:\\Users\\ribeiro\\Desktop\\PDI- 2017\\Imagenes\\escrito.jpg", image_array,compression_params );
+
+
+*MOSTRAR IMAGEN
+	
+	namedWindow("Image Array"); //ventana para mostrar la imagen
+	imshow("Image Array", image_array);
+
+------------------------------------------------------------------------------------------------------------------
